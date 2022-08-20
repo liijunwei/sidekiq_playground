098e1abe9580ab1bd49e0d5e672ca20280956d7b
lijunwei <ljw532344863@sina.com>
2022-08-20 09:09:54 +0800

    Setup sidekiq worker

---
+++ worker.rb
@@ -0,0 +1,28 @@
+require 'sidekiq'
+
+Sidekiq.configure_client do |config|
+  config.redis = {db: 1}
+end
+
+Sidekiq.configure_server do |config|
+  config.redis = {db: 1}
+end
+
+class OurWorker
+  include Sidekiq::Worker
+
+  def perform(complexity)
+    case complexity
+    when "super_hard"
+      sleep 5
+      puts "Really took a bit of effort"
+    when "hard"
+      sleep 5
+      puts "That was a bit of work"
+    else
+      sleep 1
+      puts "That wasn't a lot of effort"
+    end
+  end
+end
+
